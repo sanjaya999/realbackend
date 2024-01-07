427ab9810f16e3d04abcf3698cced3bdd7e0a3ba
Sanjaya <poudelsanjaya12@gmail.com>
2024-01-08 04:39:32 +0545

    some error in connectig db

--- src/index.js
+++ src/index.js
@@ -1,27 +1,69 @@
+
+// require('dotenv').config()
+
+import dotenv from "dotenv"
 import mongoose from "mongoose";
-import {DB_NAME, sandb} from "./constants"
+import {DB_NAME} from "./constants.js"
 import  express  from "express";
+import connectDB from "./db/index.js";
 
-const app = express()
 
-;( async ()=>{
-
-    try{
-         await mongoose.connect(`${process.env.MONGOGB_URL}/${DB_NAME}`)
-            app.on("error", ()=>{
-                console.log("error",error);
-                throw error
-            })
-            app.listen(process.env.PORT,()=>{
-                console.log(`listening`)
-            })
-    }
-    catch(error){
-        console.log("error",error)
-        throw error
-    }
-
+dotenv.config({
+    path:'./env'
 })
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express()
+
+// ;( async ()=>{
+
+//     try{
+//          await mongoose.connect(`${process.env.MONGOGB_URL}/${DB_NAME}`)
+//             app.on("error", ()=>{
+//                 console.log("error",error);
+//                 throw error
+//             })
+//             app.listen(process.env.PORT,()=>{
+//                 console.log(`listening`)
+//             })
+//     }
+//     catch(error){
+//         console.log("error",error)
+//         throw error
+//     }
+
+// })
+
+
 
 
 
