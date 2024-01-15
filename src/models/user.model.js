a1b5d3749a40656dcaa52a12dc925c7a352b098c
Sanjaya <poudelsanjaya12@gmail.com>
2024-01-16 03:13:06 +0545

    models added

---
+++ src/models/user.model.js
@@ -0,0 +1,59 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username : {
+            type:String,
+            required :true,
+            unique : true,
+            lowercase :true,
+            trim:true,
+            index:true
+        },
+
+        email : {
+            type:String,
+            required :true,
+            unique : true,
+            lowercase :true,
+            trim:true,
+            
+        },
+
+        fullname : {
+            type:String,
+            required :true,
+            trim:true,
+            index:true
+        },
+
+        avatar : {
+           type:String,
+           
+        },
+
+        coverImage:{
+            type:String,
+        },
+
+        WatchHistory:[
+            {
+            type: Schema.Types.ObjectId,
+            ref:"video",
+            }
+        ],
+
+        password:{
+            type:String,
+            required : [true, "Password is required"]
+        },
+        
+        refreshToken :{
+            type : String,
+        
+        }
+    }
+)
+        
+
+export const User = mongoose.model("User",userSchema)
