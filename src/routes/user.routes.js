3491c315b3ca812215585351e4e0a7594a2e444c
Sanjaya <poudelsanjaya12@gmail.com>
2024-01-18 17:14:47 +0545

    env updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js";
 
-const router = Router();
+const userRouter = Router();
 
 router.route("/register").post(registerUser);
 
 
-export default router
+export default userRouter
